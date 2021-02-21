f2dc7b1ababd1fc72b6bbe75d3acd5f4100313da
Munguu <mungu0623@gmail.com>
2021-02-21 12:53:19 +0800

    scroll функцийг хийж дуусагсан

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,8 @@
 import '../styles/styles.css'
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
+
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonials"), 50);
 
 let mobileMenu = new MobileMenu();
-
-
