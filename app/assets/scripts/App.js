e6fea1ab20e8b4b4aa2c59b2a7dec1a73ae09fd6
Munguu <mungu0623@gmail.com>
2021-02-21 15:54:19 +0800

    modal overly хэсэг хийж дууссан

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,10 @@ import '../styles/styles.css'
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+import Modal from "./modules/Modul";
 
 
+new Modal();
 let stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonials"), 50);
